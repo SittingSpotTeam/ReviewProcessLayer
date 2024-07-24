07a8734745c321b349fb34e0b2e4dd74cb47d2fe
davipase <davipase@gmail.com>
2024-07-24 13:29:20 +0200

    add post review

--- src/main/java/com/sittingspot/reviewprocesslayer/controllers/ReviewProcessLayerController.java
+++ src/main/java/com/sittingspot/reviewprocesslayer/controllers/ReviewProcessLayerController.java
@@ -2,18 +2,26 @@ package com.sittingspot.reviewprocesslayer.controllers;
 
 import com.sittingspot.reviewprocesslayer.models.Review;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/review-pl/api/v1")
 public class ReviewProcessLayerController {
     
     @GetMapping
-    public List<Review> getReviewById(@RequestParam String id){
+    public List<Review> getReviewById(@RequestParam UUID id){
         return List.of(); //TODO ASK REVIEW DL
+    }
+
+    @PostMapping
+    public void postReview(@RequestParam UUID id, @RequestBody String corpu){
+        //TODO
     }
 }
